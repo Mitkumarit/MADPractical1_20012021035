191a2e56ee829fcae098a3e5e9bdc100664f2f43
Mitkumarit <mitkumatpatel20@gnu.ac.in>
2022-08-06 14:08:25 +0530

    practical1.3

---
+++ practical1.3.kt
@@ -0,0 +1,29 @@
+fun main()
+{
+    println("student enrollment no :")
+    var a= readLine()!!.toInt()
+    println("student name :")
+    var b= readLine()!!
+    println("student branch :")
+    var c= readLine()!!
+    println("student class :")
+    var d= readLine()!!
+    println("student batch :")
+    var e= readLine()!!
+    println("student college name :")
+    var f= readLine()!!
+    println("student university name :")
+    var i= readLine()!!
+    println("student age :")
+    var j= readLine()!!.toInt()
+
+    println("student enrollment no :$a")
+    println("student name :$b")
+    println("student branch :$c")
+    println("student class :$d")
+    println("student batch :$e")
+    println("student college name :$f")
+    println("student university name :$i")
+    println("student age :$j")
+
+}
