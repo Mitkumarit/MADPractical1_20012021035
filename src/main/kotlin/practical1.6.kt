731096d520d0cbbc4e3379dd526332b6318a3f08
Mitkumarit <mitkumatpatel20@gnu.ac.in>
2022-08-06 15:26:19 +0530

    practical1(6)

---
+++ src/main/kotlin/practical1.6.kt
@@ -0,0 +1,12 @@
+fun main(){
+    val x= readLine()!!.toInt()
+    val y= readLine()!!.toInt()
+    val z= readLine()!!.toInt()
+
+    val a=addnumber(x,y,z)
+    println("the addition of the number is: $a")
+}
+
+fun addnumber(vararg, arrya:Int):Int{
+
+}
