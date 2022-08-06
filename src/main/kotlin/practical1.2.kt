dd918bdceb19543cd4016d7aad6085a08253c166
Mitkumarit <mitkumatpatel20@gnu.ac.in>
2022-08-06 14:28:42 +0530

    practical1(2)

--- src/main/kotlin/practical1.2.kt
+++ src/main/kotlin/practical1.2.kt
@@ -1,10 +1,10 @@
 fun main(){
-    var x:Int=10
-    var y:Double=x.toDouble()
-    var z: String=x.toString()
-    var a:Int=z.toInt()
-    var f:String="11.23"
-    var b:Double=f.toDouble()
+    val x:Int=10
+    val y:Double=x.toDouble()
+    val z: String=x.toString()
+    val a:Int=z.toInt()
+    val f:String="11.23"
+    val b:Double=f.toDouble()
     println("Integer value: $x")
     println("Double value (From Integer): $y")
     println("String value: $z")
