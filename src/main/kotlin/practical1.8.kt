11c93199af7ef1a8432cf6acacb85ff5641da705
DELL <mitkumarpatel20@gnu.ac.in>
2022-08-23 21:15:45 +0530

    practical1(8)

--- src/main/kotlin/practical1.8.kt
+++ src/main/kotlin/practical1.8.kt
@@ -1,68 +1,75 @@
 fun main(){
     /*----------------1 type------------*/
-    val myarray1 = arrayOf(9,6,7,6,7,8)
-    println(myarray1.contentDeepToString())
+    val myarray1 = arrayOf(1,2,3,4)
+    println(myarray1.contentToString())
+    /*----------------1 type------------*/
 
-    /*--------------- 2 type -----------*/
-    val myarray2 = ArrayList<String>()
-    myarray2.add("5")
-    myarray2.add("4")
-    myarray2.add("2")
-    myarray2.add("7")
-    myarray2.add("9")
-    println(myarray2)
 
-    /*--------------- 3 type -----------
 
-    val myarray3:(String,String,String,String)-> String={}
-    val array=myarray3("meet","jay","radhe","shyam")
+    /*----------------2 type------------*/
+    val myarray2= arrayOf<Int>(1,2,3,4,5)
+    println(myarray2.contentToString())
+    /*----------------2 type------------*/
 
-    --------------- 4 type -----------*/
 
-    val arr4 = IntArray(5) { 1 }
-    println(arr4.contentToString())
 
-    /*--------------- 5 type -----------*/
-    var arr5: IntArray = intArrayOf(5,10,20,12,15)
-    println(arr5.contentToString())
+    /*----------------3 type------------*/
+    val myarray3= Array(5,{i -> i*1 })
+    println(myarray3.contentToString())
+    /*----------------3 type------------*/
 
-    /*--------------- 5 type -----------*/
-    var arr = arrayOf(intArrayOf(1, 2), intArrayOf(4, 5), intArrayOf(7, 8))
-    for(i in arr){
-        println(i.contentToString())
+    /*----------------4 type------------*/
+    val myarray4 = intArrayOf(1, 2, 3, 4)
+    println(myarray4.contentToString())
+    /*----------------4 type------------*/
+
+    /*----------------5 type------------*/
+    val myarray5 =IntArray(5) { 10 * (it + 1) }
+    println(myarray5.contentToString())
+    /*----------------5 type------------*/
+
+    /*----------------6 type------------*/
+    val myarray6 = arrayOf(intArrayOf(1, 2),intArrayOf(3, 4),intArrayOf(5, 6))
+    println(myarray6.contentDeepToString())
+    /*----------------6 type------------*/
+
+
+
+    /*------------------------------------------------------------------------------*/
+
+    println("Please Enter The Array Values")
+    println("size: ")
+    val size= readLine()!!.toInt()
+    val myarray7= arrayListOf<Int>()
+    for (i in 0..size-1){
+        println("a[$i] : ")
+        myarray7.add(readLine()!!.toInt())
+
     }
+    println("entered array is : ")
+    println(myarray7.toString())
+    println("************With Built-in Function*************")
+    println("**********After sorting by built-in function**********")
+    myarray7.sort()
+    println(myarray7.toString())
 
-
-
-
-
-
-
-
-
-
-
-    println("enter the array value:")
-    println("a[0]:")
-    val val1= readLine()!!
-    println("a[1]:")
-    val val2= readLine()!!
-    println("a[2]:")
-    val val3= readLine()!!
-    println("a[3]:")
-    val val4= readLine()!!
-    println("a[4]:")
-    val val5= readLine()!!
-
-    val myarray = arrayOf("$val1","$val2","$val3","$val4","$val5")
-    println(myarray.contentDeepToString())
-
-    var largest = myarray[0]
-
-    for (num in myarray) {
-        if (largest < num)
-            largest = num
+    println("************without built-in function**********")
+    println("***********After Sorting**********")
+    var temp: Int
+    for (i in 0..myarray7.size-1) {
+        for (j in i+1..myarray7.size-1)
+        {
+            if (myarray7[i] > myarray7[j])
+            {
+                temp = myarray7[i]
+                myarray7[i] = myarray7[j]
+                myarray7[j] = temp
+            }
+        }
     }
-    println(largest)
+    println(myarray7.toString())
+
+    /*------------------------------------------------------------------------------*/
+
 
 }
